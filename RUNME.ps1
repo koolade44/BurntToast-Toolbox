6ac0a4b86bd25c00aec4f13c700a94b55363a23f
koolade44 <50341448+koolade44@users.noreply.github.com>
2020-08-31 20:58:21 -0600

    Update RUNME.ps1

--- RUNME.ps1
+++ RUNME.ps1
@@ -8,6 +8,5 @@ else {
 }
 New-Item -Path "C:\Users\$Env:USERNAME" -Name "Icons" -ItemType "Directory"
 Move-Item -Path ".\cmdscr\*.ps1" -Destination "C:\Users\$Env:USERNAME"
+Move-Item -Path ".\cmdscr\check.png" -Destination "C:\Users\$Env:USERNAME\Icons"
 Remove-Item -Path ".\cmdscr" -Recurse
-
-./DownloadAsIcon "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.rsrit.com%2Fwp-content%2Fuploads%2F2017%2F12%2Fcheck-1-icon.png&imgrefurl=https%3A%2F%2Fwww.rsrit.com%2Finformation-management%2Fattachment%2Fcheck-1-icon%2F&tbnid=IqvulzH9fZWBVM&vet=12ahUKEwjlxYDh-cbrAhWbAJ0JHeKLBHMQMygIegUIARDfAQ..i&docid=ihvVOrcR4PgJyM&w=256&h=256&q=icon&hl=en&ved=2ahUKEwjlxYDh-cbrAhWbAJ0JHeKLBHMQMygIegUIARDfAQ" check
