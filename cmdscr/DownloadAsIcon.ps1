651928fd6c0e3a926164a00242d2365f8069c63e
koolade44 <50341448+koolade44@users.noreply.github.com>
2020-08-31 20:55:37 -0600

    Add files via upload

---
+++ cmdscr/DownloadAsIcon.ps1
@@ -0,0 +1,8 @@
+$URL = $args[0]
+$Name = $args[1] + ".png"
+$Site = iwr -Uri $URL
+$Images = ($Site).Images.src
+foreach ($Image in $Images) {
+	Start-BitsTransfer -Source $Image -Destination "C:\Users\Student\Icons\$Name" -TransferType Download
+}
+New-BurntToastNotification -AppLogo "C:\Users\$Env:USERNAME\Icons\check.png" -Text "Image Downloaded", "Image successfully downloaded"
